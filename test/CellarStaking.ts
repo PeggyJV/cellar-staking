1e77ddbe87a52f9f2f93c3211eab9515740e491e
Elliot Shiu <sandbochs@users.noreply.github.com>
2022-01-13 16:44:54 +0900

    chore(test): assert stake error

--- test/CellarStaking.ts
+++ test/CellarStaking.ts
@@ -19,8 +19,6 @@ interface TestContext {
   staking: CellarStaking;
 }
 
-const ONE_DAY = 60 * 60 * 24;
-
 describe("CellarStaking", () => {
   let ctx: TestContext;
 
@@ -60,60 +58,60 @@ describe("CellarStaking", () => {
         const { staking, user } = ctx;
 
         const stakingAsUser = await staking.connect(user);
-        await stakingAsUser.stake(1000, 0);
+        await expect(stakingAsUser.stake(1000, 0)).to.be.revertedWith("STATE_NotInitialized");
       });
-      // it("should not allow a user to stake if the stake in under the minimum");
-      // it("should not allow a user to stake if there are no rewards left");
-      // it("should not allow a user to stake if their stake is too small to receive a share");
-      // it("should revert for an invalid lock value");
-      // it("should allow a user to stake and calculate correct proportional share");
-      // it("should properly calculate a user's proportional share after locking boosts");
-      // it("should allocate correct proportional shares for multiple depositors");
+      it("should not allow a user to stake if the stake in under the minimum");
+      it("should not allow a user to stake if there are no rewards left");
+      it("should not allow a user to stake if their stake is too small to receive a share");
+      it("should revert for an invalid lock value");
+      it("should allow a user to stake and calculate correct proportional share");
+      it("should properly calculate a user's proportional share after locking boosts");
+      it("should allocate correct proportional shares for multiple depositors");
     });
-    //
-    // describe("unstake", () => {
-    //   it("should not allow unstaking if the rewards are not initialized");
-    //   it("should require a non-zero amount to unstake");
-    //   it("should revert if the depositId is invalid");
-    //   it("should not allow unstaking a stake that is still locked");
-    //   it("should not unstake more than the deposited amount");
-    //   it("should not allow a user to unstake an amount smaller than the unit share size");
-    //   it("should unstake, distributing both the specified deposit amount and any accumulated rewards");
-    // });
-    //
-    // describe("unstakeAll", () => {
-    //   it("should unstake all amounts for all deposits, and distribute all available rewards");
-    // });
-    //
-    // describe("claim", () => {
-    //   it("should not allow claiming if the rewards are not initialized");
-    //   it("claim available rewards for a given deposit");
-    //   it("should correctly calculate rewards for two claims within the same epoch");
-    //   it("should correctly calculate rewards across multiple epochs");
-    //   it("should corretctly calculate proportional rewards across different user's stakes, in the same epoch");
-    //   it(
-    //     "should correctly calculate proportional rewards for different user stakes, deposited during different epochs",
-    //   );
-    //   it("should not redistribute rewards tha have already been claimed");
-    // });
-    //
-    // describe("claimAll", () => {
-    //   it("should claim all available rewards for all deposits, within the same epoch");
-    //   it("should claim all available rewards for all deposits, across multiple epochs");
-    // });
-    //
-    // describe("emergencyUnstake", () => {
-    //   it("should revert if the staking program has not been ended");
-    //   it("should return all staked tokens, across multiple stakes, regardless of lock status");
-    // });
-    //
-    // describe("emergencyClaim", () => {
-    //   it("should revert if the staking program has not been ended");
-    //   it("should return all staked tokens and distribute all unclaimed rewards");
-    // });
+
+    describe("unstake", () => {
+      it("should not allow unstaking if the rewards are not initialized");
+      it("should require a non-zero amount to unstake");
+      it("should revert if the depositId is invalid");
+      it("should not allow unstaking a stake that is still locked");
+      it("should not unstake more than the deposited amount");
+      it("should not allow a user to unstake an amount smaller than the unit share size");
+      it("should unstake, distributing both the specified deposit amount and any accumulated rewards");
+    });
+
+    describe("unstakeAll", () => {
+      it("should unstake all amounts for all deposits, and distribute all available rewards");
+    });
+
+    describe("claim", () => {
+      it("should not allow claiming if the rewards are not initialized");
+      it("claim available rewards for a given deposit");
+      it("should correctly calculate rewards for two claims within the same epoch");
+      it("should correctly calculate rewards across multiple epochs");
+      it("should corretctly calculate proportional rewards across different user's stakes, in the same epoch");
+      it(
+        "should correctly calculate proportional rewards for different user stakes, deposited during different epochs",
+      );
+      it("should not redistribute rewards tha have already been claimed");
+    });
+
+    describe("claimAll", () => {
+      it("should claim all available rewards for all deposits, within the same epoch");
+      it("should claim all available rewards for all deposits, across multiple epochs");
+    });
+
+    describe("emergencyUnstake", () => {
+      it("should revert if the staking program has not been ended");
+      it("should return all staked tokens, across multiple stakes, regardless of lock status");
+    });
+
+    describe("emergencyClaim", () => {
+      it("should revert if the staking program has not been ended");
+      it("should return all staked tokens and distribute all unclaimed rewards");
+    });
   });
 
-  describe.skip("Admin Operations", () => {
+  describe("Admin Operations", () => {
     describe("initializePool", () => {
       it("should revert if caller is not the owner");
       it("should revert if the contract is paused");
@@ -157,7 +155,7 @@ describe("CellarStaking", () => {
     });
   });
 
-  describe.skip("State Information", () => {
+  describe("State Information", () => {
     describe("currentEpoch", () => {
       it("should report the correct epoch for the current time");
       it("should revert if there is no active epoch");
